17bcfa93903b0030fb90b57545c421c0edd0f448
john <luca.kebbel@yahoo.de>
2020-04-04 17:34:47 +0200

    added change function

--- database/database.js
+++ database/database.js
@@ -1,11 +1,11 @@
-const sqlite3 = require("sqlite3").verbose();
-const path = require("path");
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
 
-const dbPath = path.resolve(__dirname, "data.db");
+const dbPath = path.resolve(__dirname, 'data.db');
 
 const getDb = () => {
-  console.log("starting db", dbPath);
-  return new sqlite3.Database(dbPath);
+	console.log('db');
+	return new sqlite3.Database(dbPath);
 };
 
 module.exports = getDb;
--- routes/editTask/index.js
+++ routes/editTask/index.js
@@ -1,17 +1,24 @@
-const express = require("express");
+const express = require('express');
 
-const dbFunc = require("../../database/database.js");
+const dbFunc = require('../../database/database.js');
 
 const router = express.Router();
 
-const editTask = (req, res) => {    
-
-    const db = dbFunc();
-    const body = req.body;
-    console.log(params,body);
-    res.send("ok",body);
+const editTask = (req, res) => {
+	const db = dbFunc();
+	const { task } = req.body;
+	const sql = `
+    UPDATE tasks
+    SET 
+    name = ? ,
+    description = ?,
+    urgency = ?,
+    done= ?
+    WHERE ID = ${task.ID} `;
+	db.run(sql, [ task.name, task.description, task.urgency, task.done ]);
+	res.send('ok');
 };
 
-router.put("/editTask", editTask);
+router.put('/editTask', editTask);
 
 module.exports = router;
