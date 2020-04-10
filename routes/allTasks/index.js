c400a9f9802503ab86dbefca2ba1fea8a072d1b3
john <luca.kebbel@yahoo.de>
2020-04-10 11:24:13 +0200

    desc

--- routes/allTasks/index.js
+++ routes/allTasks/index.js
@@ -1,30 +1,29 @@
-const express = require("express");
-const dbFunc = require("../../database/database.js");
+const express = require('express');
+const dbFunc = require('../../database/database.js');
 const router = express.Router();
 
 const tasks = (req, res) => {
-    const db = dbFunc();
+	const db = dbFunc();
 
-    let respArray = [];
-    
-    db.serialize(() => {
-      db.each(
-        "SELECT * FROM tasks order by urgency asc",
-        (err, row) => {
-          respArray.push(row);
-        },
-        (err) => {
-          if (err) {
-            res.send("error getting games");
-          }
-          
-          res.send(JSON.stringify(respArray));
-        },
-      );
-    });
-  
-      
-  };
-  router.get("/tasks", tasks);
+	let respArray = [];
+
+	db.serialize(() => {
+		db.each(
+			'SELECT * FROM tasks order by urgency desc',
+			(err, row) => {
+				respArray.push(row);
+			},
+			(err) => {
+				if (err) {
+					console.log(resp);
+					res.send('error getting games');
+				}
+
+				res.send(JSON.stringify(respArray));
+			}
+		);
+	});
+};
+router.get('/tasks', tasks);
 
 module.exports = router;
